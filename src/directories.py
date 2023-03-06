d26dcf18918921b09ba1966aad20b623441d4143
AntaloFis <antalofisica@gmail.com>
2023-03-06 11:45:55 +0100

    init repo

--- src/directories.py
+++ src/directories.py
@@ -5,16 +5,16 @@ dir_arrays_TTV = 'data/arrays Train-Val-Test/'
 dir_models = 'models/notebooks/'
 
 # Tensorboard logs
-dir_TB_logs = 'models/TBlogs/'
+# dir_TB_logs = 'models/TBlogs/'
 
 # Images
 dir_noise_crop50 = 'data/images/noise/crop 50x50/'
 dir_signal_crop50 = 'data/images/signal/crop 50x50/'
-dir_signal_orig = 'data/images/signal/crop originals/'
-dir_sum_test_2_crop50 = 'data/images/sumadas/test 2/crop 50x50/'
-dir_test_2_crop50 = 'data/images/test 2/crop 50x50/'
-dir_test_2_orig = 'data/images/test 2/originals/'
-dir_infer = 'data/images/inferencia/'
+# dir_signal_orig = 'data/images/signal/crop originals/'
+# dir_sum_test_2_crop50 = 'data/images/sumadas/test 2/crop 50x50/'
+# dir_test_2_crop50 = 'data/images/test 2/crop 50x50/'
+# dir_test_2_orig = 'data/images/test 2/originals/'
+dir_infer = 'data/images/inference/'
 
 # Exports
-dir_exports_img = 'exports/images/'
+# dir_exports_img = 'exports/images/'
