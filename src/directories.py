303bed545c20dde86285fdbc64b9a500ee3684dd
AntaloFis <antalofisica@gmail.com>
2023-03-06 10:52:38 +0100

    initial prototype CNN - dockerization init

---
+++ src/directories.py
@@ -0,0 +1,20 @@
+# Arrays
+dir_arrays_TTV = 'data/arrays Train-Val-Test/'
+
+# Models
+dir_models = 'models/notebooks/'
+
+# Tensorboard logs
+dir_TB_logs = 'models/TBlogs/'
+
+# Images
+dir_noise_crop50 = 'data/images/noise/crop 50x50/'
+dir_signal_crop50 = 'data/images/signal/crop 50x50/'
+dir_signal_orig = 'data/images/signal/crop originals/'
+dir_sum_test_2_crop50 = 'data/images/sumadas/test 2/crop 50x50/'
+dir_test_2_crop50 = 'data/images/test 2/crop 50x50/'
+dir_test_2_orig = 'data/images/test 2/originals/'
+dir_infer = 'data/images/inferencia/'
+
+# Exports
+dir_exports_img = 'exports/images/'
